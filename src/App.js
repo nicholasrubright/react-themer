e86f1f0e36b89bc79ed5b3375e9ff061edef4d10
Nicholas <koalrube@gmail.com>
2023-09-02 11:30:53 -0400

    added more stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 const App = () => {
-    return <h1>Hello React</h1>;
+
+    return (
+        <div>
+            <Header />
+            <div>
+                <h1>Content!!</h1>
+            </div>
+            <Footer />
+        </div>
+    )
 }
 
 export default App;
